04348de2f797f98ef789df77e9fdcf16034cde39
pandanoir <moon_in_france@yahoo.co.jp>
2020-01-20 16:08:34 +0900

    remove fish configs

--- fish/functions/fvim.fish
+++
@@ -1,5 +0,0 @@
-function fvim
-    set -l files (fzf-tmux)
-    [ -n "$files" ]; and vim $files
-end
-
--- fish/functions/vimf.fish
+++
@@ -1,5 +0,0 @@
-function vimf
-    set -l files (fzf-tmux)
-    [ -n "$files" ]; and vim $files
-end
-
