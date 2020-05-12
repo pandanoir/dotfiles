813899a0f6caf6723fadbdad0e3d310ef8a2ba5d
pandanoir <moon_in_france@yahoo.co.jp>
2020-05-12 22:32:52 +0900

    launch tmux when shell is not executed from VSCode

--- zsh/launch_tmux.zsh
+++ zsh/launch_tmux.zsh
@@ -3,11 +3,10 @@
 # 1. SSH先にすでにattachしたセッションが存在する -> detach済みセッションがあればattach、なければnew-session
 # 1. ローカルで新しく端末を立ち上げる(=すでにattachしたセッションが存在する) -> tmuxを起動したくない
 # -> ローカルとSSH先を区別する必要がある
+
 launch_tmux() {
-    if [ `whoami` != 'root' -a -z "$TMUX" -a -z "$STY" ]; then
-        if type tmuxx >/dev/null 2>&1; then
-            tmuxx
-        elif type tmux >/dev/null 2>&1; then
+    if [ `whoami` != 'root' -a -z "$TMUX" -a -z "$STY" -a ! -v IS_VSCODE ]; then
+        if type tmux >/dev/null 2>&1; then
             if tmux has-session; then
                 if [[ -n `tmux list-sessions | grep '(attached)'` ]]; then
                     # アタッチ済み
