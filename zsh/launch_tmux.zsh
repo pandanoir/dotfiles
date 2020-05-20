7d705413ec5d5f6f944ea149878e2b662ec8e8b6
pandanoir <moon_in_france@yahoo.co.jp>
2020-05-20 23:40:11 +0900

    Refactor launch_tmux.zsh

--- zsh/launch_tmux.zsh
+++ zsh/launch_tmux.zsh
@@ -4,33 +4,29 @@
 # 1. ローカルで新しく端末を立ち上げる(=すでにattachしたセッションが存在する) -> tmuxを起動したくない
 # -> ローカルとSSH先を区別する必要がある
 
+source "$ZDOTDIR/utils.zsh"
 launch_tmux() {
     if [ `whoami` = 'root' ] || ! is_empty_string "$TMUX" || ! is_empty_string "$STY" || [ -v IS_VSCODE ]; then
         return
     fi
 
-    if ! type tmux >/dev/null 2>&1; then
-        if type screen >/dev/null 2>&1; then
-            screen -rx || screen -D -RR
-        fi
+    if ! has tmux; then
+        # そもそもtmuxがない
         return
     fi
     if ! tmux has-session; then
-        exec tmux new-session && echo "tmux created new session"
+        exec tmux new-session # セッションがない
     fi
-    if ! [[ -n `tmux list-sessions | grep '(attached)'` ]]; then
-        exec tmux attach && echo "tmux attached session"
+    if is_empty_string "$(tmux list-sessions | grep '(attached)')"; then
+        exec tmux attach # アタッチされていないセッションがある
     fi
-    # アタッチ済み
+
     if [ -v NOT_NEW_SESSION_TMUX ]; then
+        # 新しいセッションを立ち上げたくない
         echo "detached tmux session doesn't exist"
         return
     fi
-    if [[ -n `tmux list-sessions | grep -v '(attached)'` ]]; then
-        # デタッチ済みセッションがある
-        exec tmux attach && echo "tmux attached session"
-    else
-        exec tmux new-session && echo "tmux created new session"
-    fi
+
+    exec tmux new-session
 }
 launch_tmux
