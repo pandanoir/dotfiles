069976472517fa4300058e8ff45e548e3928ca89
pandanoir <moon_in_france@yahoo.co.jp>
2020-05-20 23:19:22 +0900

    Reduce indent in the launch_tmux.zsh

--- zsh/launch_tmux.zsh
+++ zsh/launch_tmux.zsh
@@ -5,28 +5,32 @@
 # -> ローカルとSSH先を区別する必要がある
 
 launch_tmux() {
-    if [ `whoami` != 'root' -a -z "$TMUX" -a -z "$STY" -a ! -v IS_VSCODE ]; then
-        if type tmux >/dev/null 2>&1; then
-            if tmux has-session; then
-                if [[ -n `tmux list-sessions | grep '(attached)'` ]]; then
-                    # アタッチ済み
-                    if [ ! -v NOT_NEW_SESSION_TMUX ]; then
-                        if [[ -n `tmux list-sessions | grep -v '(attached)'` ]]; then
-                            # デタッチ済みセッションがある
-                            exec tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf attach && echo "tmux attached session"
-                        else
-                            exec tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf new-session && echo "tmux created new session"
-                        fi
-                    fi
-                else
-                    exec tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf attach && echo "tmux attached session"
-                fi
-            else
-                exec tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf new-session && echo "tmux created new session"
-            fi
-        elif type screen >/dev/null 2>&1; then
+    if [ `whoami` = 'root' ] || ! is_empty_string "$TMUX" || ! is_empty_string "$STY" || [ -v IS_VSCODE ]; then
+        return
+    fi
+
+    if ! type tmux >/dev/null 2>&1; then
+        if type screen >/dev/null 2>&1; then
             screen -rx || screen -D -RR
         fi
+        return
+    fi
+    if ! tmux has-session; then
+        exec tmux new-session && echo "tmux created new session"
+    fi
+    if ! [[ -n `tmux list-sessions | grep '(attached)'` ]]; then
+        exec tmux attach && echo "tmux attached session"
+    fi
+    # アタッチ済み
+    if [ -v NOT_NEW_SESSION_TMUX ]; then
+        echo "detached tmux session doesn't exist"
+        return
+    fi
+    if [[ -n `tmux list-sessions | grep -v '(attached)'` ]]; then
+        # デタッチ済みセッションがある
+        exec tmux attach && echo "tmux attached session"
+    else
+        exec tmux new-session && echo "tmux created new session"
     fi
 }
 launch_tmux
