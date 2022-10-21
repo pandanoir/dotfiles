16f72f3500952cd25fbbf99b737dede4a446f68d
pandanoir <moon_in_france@yahoo.co.jp>
2022-10-21 09:42:13 +0900

    add rgf alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,9 +2,9 @@ alias mv='mv -i'
 alias cp='cp -i'
 alias cdb='cd-bookmark'
 if has pbcopy; then
-    alias copy="pbcopy"
+  alias copy="pbcopy"
 else
-    alias copy='xsel --clipboard --input'
+  alias copy='xsel --clipboard --input'
 fi
 alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
 
@@ -32,17 +32,17 @@ gro() {
   git rebase origin/$(git symbolic-ref --short HEAD)
 }
 git-rm-merged-branch() {
-  git branch --merged | grep -v '^*' | grep -v 'master' | xargs git branch -d
+git branch --merged | grep -v '^*' | grep -v 'master' | xargs git branch -d
 }
 git-set-upstream-branch() {
-  git branch --set-upstream-to="origin/$(git branch --show-current)"
+git branch --set-upstream-to="origin/$(git branch --show-current)"
 }
 
 fcs() {
   local commits commit
   commits=$(git --no-pager log --color=always --all --oneline --abbrev-commit --decorate=full 2>&1) &&
-  commit=$(echo "$commits" | fzf +s +m -e --ansi --reverse) &&
-  echo -n $(echo "$commit" | sed "s/ .*//")
+    commit=$(echo "$commits" | fzf +s +m -e --ansi --reverse) &&
+    echo -n $(echo "$commit" | sed "s/ .*//")
 }
 fbr() {
   git branch | fzf +s +m -e --ansi --reverse | sed -e 's/^ *//' -e 's/^\* //'
@@ -79,25 +79,28 @@ alias vi="env -u VIM env VIMINIT=':source $XDG_CONFIG_HOME'/vim/vimrc vim"
 alias vim="env -u VIM env VIMINIT=':source $XDG_CONFIG_HOME'/vim/vimrc vim"
 
 if command_exists nvim; then
-    alias vim="nvim"
+  alias vim="nvim"
 fi
 if command_exists exa; then
-    alias ls="exa --group-directories-first"
-    alias ll='exa --group-directories-first -algh --git'
-    alias lg='exa --group-directories-first --git-ignore'
-    function chpwd() { exa --group-directories-first }
+  alias ls="exa --group-directories-first"
+  alias ll='exa --group-directories-first -algh --git'
+  alias lg='exa --group-directories-first --git-ignore'
+  function chpwd() { exa --group-directories-first }
 else
-    if is_mac; then
-        alias ls='ls -G'
-        alias ll='ls -l -G'
-    else
-        alias ls='ls --color=always'
-        alias ll='ls -l --color=always'
-    fi
-    alias lg='exa --git-ignore'
+  if is_mac; then
+    alias ls='ls -G'
+    alias ll='ls -l -G'
+  else
+    alias ls='ls --color=always'
+    alias ll='ls -l --color=always'
+  fi
+  alias lg='exa --git-ignore'
 fi
 if command_exists bat; then alias cat='bat'; fi
 if command_exists ranger; then
-    alias ra="ranger"
-    alias rang="ranger"
+  alias ra="ranger"
+  alias rang="ranger"
+fi
+if command_exists rg; then
+  alias rgf="rg --files | rg"
 fi
