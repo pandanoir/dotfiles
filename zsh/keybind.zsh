30be4628bcb7459d9376a179df5be5fce857a780
pandanoir <moon_in_france@yahoo.co.jp>
2023-03-31 21:13:38 +0900

    add fzf-git-edited-widget

--- zsh/keybind.zsh
+++ zsh/keybind.zsh
@@ -17,5 +17,23 @@ fancy-ctrl-z () {
 }
 zle -N fancy-ctrl-z
 bindkey '^Z' fancy-ctrl-z
-
 bindkey '^W' fzf-file-widget
+
+__fsel2 () {
+    local item
+    fzf +s +m -e --multi --ansi --reverse --height 40% --preview='git diff --color $('"echo {} | awk '{print substr(\$0,4)}') | tail -n +5" |
+    while read item; do
+        echo -n "${(q)item} "
+    done
+    local ret=$?
+    echo
+    return $ret
+}
+fzf-git-edited-widget() {
+    LBUFFER="${LBUFFER}$(__fsel2)"
+    local ret=$?
+    zle reset-prompt
+    return $ret
+}
+zle -N fzf-git-edited-widget
+bindkey '^X^W' fzf-git-edited-widget
