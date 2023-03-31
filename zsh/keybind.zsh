54a294ad9b255ee3e2e30ad15af1c4fef9cb626f
pandanoir <moon_in_france@yahoo.co.jp>
2023-03-31 21:21:28 +0900

    fix fsel2

--- zsh/keybind.zsh
+++ zsh/keybind.zsh
@@ -19,9 +19,12 @@ zle -N fancy-ctrl-z
 bindkey '^Z' fancy-ctrl-z
 bindkey '^W' fzf-file-widget
 
-__fsel2 () {
+__fsel2() {
     local item
+
+    git status --porcelain --untracked-files |
     fzf +s +m -e --multi --ansi --reverse --height 40% --preview='git diff --color $('"echo {} | awk '{print substr(\$0,4)}') | tail -n +5" |
+    awk '{print substr($0,4)}' |
     while read item; do
         echo -n "${(q)item} "
     done
