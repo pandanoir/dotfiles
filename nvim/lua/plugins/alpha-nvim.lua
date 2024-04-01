28ccbbf4ae5191333f953d70a4ad7bbda927cff7
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2024-04-01 09:47:15 +0900

    change keybind of new file in alpha-nvim

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -8,7 +8,7 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button('t', '  nvim-tree', ':NvimTreeToggle<cr>'),
       dashboard.button('f', ' ' .. ' Find file', '<cmd> Telescope find_files <cr>'),
-      dashboard.button('n', ' ' .. ' New file', '<cmd> ene <BAR> startinsert <cr>'),
+      dashboard.button('i', ' ' .. ' New file', '<cmd> ene <BAR> startinsert <cr>'),
       dashboard.button('r', ' ' .. ' Recent files', '<cmd> Telescope oldfiles <cr>'),
       dashboard.button('g', ' ' .. ' Find text', '<cmd> Telescope live_grep <cr>'),
       dashboard.button('l', '󰒲 ' .. ' Lazy', '<cmd> Lazy <cr>'),
