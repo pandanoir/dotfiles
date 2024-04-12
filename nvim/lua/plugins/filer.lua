3ff1c92eb124c7367259225077afa535bbae2146
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2024-04-12 09:37:21 +0900

    use nvim-tree instead of netrw

--- nvim/lua/plugins/filer.lua
+++ nvim/lua/plugins/filer.lua
@@ -1,13 +1,10 @@
 return {
   'nvim-tree/nvim-tree.lua',
-  cmd = 'NvimTreeToggle',
-  keys = { { '<leader>s', ':<C-u>NvimTreeToggle<CR>' } },
   init = function()
-    -- disable netrw at the very start of your init.lua (strongly advised)
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+    vim.keymap.set('n', '<leader>s', ':<C-u>NvimTreeToggle<CR>')
   end,
   opts = {
+    hijack_netrw = true,
     update_focused_file = {
       enable = true,
     },
