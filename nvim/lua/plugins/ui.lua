937b2ef26e4835b5c43a9a86434da5fc5ff22ddf
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2024-05-30 21:23:20 +0900

    change style of lualine

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -51,16 +51,70 @@ return {
     'nvim-lualine/lualine.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
-    opts = {
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-      },
-    },
+    opts = function()
+      local empty = require('lualine.component'):extend()
+      function empty:draw(default_highlight)
+        self.status = ''
+        self.applied_separator = ''
+        self:apply_highlights(default_highlight)
+        self:apply_section_separators()
+        return self.status
+      end
+
+      local colors = {
+        grey = '#333344',
+      }
+
+      -- Put proper separators and gaps between components in sections
+      local function process_sections(sections)
+        for name, section in pairs(sections) do
+          local left = name:sub(9, 10) < 'x'
+          for pos = 1, name ~= 'lualine_z' and #section or #section - 1 do
+            table.insert(section, pos * 2, { empty, color = { fg = colors.grey, bg = colors.grey } })
+          end
+          for id, comp in ipairs(section) do
+            if type(comp) ~= 'table' then
+              comp = { comp }
+              section[id] = comp
+            end
+            comp.separator = left and { right = '' } or { left = '' }
+          end
+        end
+        return sections
+      end
+
+      return {
+        options = {
+          component_separators = '',
+          section_separators = { left = '', right = '' },
+        },
+        sections = process_sections {
+          lualine_a = { 'mode' },
+          lualine_b = {
+            'branch',
+            {
+              'diagnostics',
+              source = { 'nvim' },
+              sections = { 'error' },
+            },
+            {
+              'diagnostics',
+              source = { 'nvim' },
+              sections = { 'warn' },
+            },
+            { 'filename', file_status = false },
+          },
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = { 'filetype' },
+          lualine_z = { '%l:%c', '%p%%/%L' },
+        },
+        inactive_sections = {
+          lualine_c = { '%f %y %m' },
+          lualine_x = {},
+        },
+      }
+    end,
   },
   {
     'navarasu/onedark.nvim',
