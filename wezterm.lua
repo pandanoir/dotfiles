a42852d987e0244af0889d4d4e5ff55c0830cd87
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2024-05-30 21:36:06 +0900

    improve wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,18 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.font_size = 13
-
+config.font = wezterm.font_with_fallback {
+  { family = 'Hack Nerd Font Mono', assume_emoji_presentation = false },
+  { family = 'Hack Nerd Font Mono', assume_emoji_presentation = true },
+  { family = 'ヒラギノ角ゴシック' },
+}
+config.font_size = 15
+config.color_scheme = 'Tokyo Night'
+config.keys = {
+  { key = 'LeftArrow',  mods = 'OPT', action = wezterm.action { SendString = '\x1bb' } },
+  { key = 'RightArrow', mods = 'OPT', action = wezterm.action { SendString = '\x1bf' } },
+}
+config.initial_rows = 60
+config.initial_cols = 160
+  
 return config
