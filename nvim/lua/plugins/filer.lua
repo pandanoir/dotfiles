1c250905f4cfb63b609f4516523acb0ca6469b69
Naoto Ikuno <pandanoir@ymail.ne.jp>
2024-05-30 21:12:46 +0900

    show hidden files in oil

--- nvim/lua/plugins/filer.lua
+++ nvim/lua/plugins/filer.lua
@@ -1,6 +1,9 @@
 return {
   'stevearc/oil.nvim',
-  opts = { float = { padding = 4, max_width = 150 } },
+  opts = {
+    float = { padding = 4, max_width = 150 },
+    view_options = { show_hidden = true },
+  },
   init = function()
     vim.keymap.set('n', '<leader>s', ':<C-u>Oil --float<CR>')
     vim.api.nvim_create_autocmd({ 'FileType' }, {
