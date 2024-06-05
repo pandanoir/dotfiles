993998d847c9ae95bd521b394723239fdd714ac3
pandanoir <pandanoir@ymail.ne.jp>
2024-06-05 09:40:38 +0900

    setup nvim-ufo and remove pretty-fold and fold-preview

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -11,15 +11,6 @@ return {
   --   end
   -- },
   {
-    'anuvyklack/pretty-fold.nvim',
-    config = true,
-  },
-  {
-    'anuvyklack/fold-preview.nvim',
-    dependencies = { 'anuvyklack/keymap-amend.nvim' },
-    config = true,
-  },
-  {
     'chentoast/marks.nvim',
     event = 'BufRead',
     opts = {
@@ -41,6 +32,16 @@ return {
         return { 'treesitter', 'indent' }
       end
     },
+    init = function()
+      vim.o.foldcolumn = '0' -- '0' is not bad
+      vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require 'ufo'.openAllFolds)
+      vim.keymap.set('n', 'zM', require 'ufo'.closeAllFolds)
+    end
   },
   {
     'shellRaining/hlchunk.nvim',
