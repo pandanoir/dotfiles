4e78d0b6dcc0aad8ed8866a57527f042f9904f2d
pandanoir <pandanoir@ymail.ne.jp>
2024-06-06 06:04:35 +0900

    install nvim-treesitter-textsubjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   event = 'VeryLazy',
-  build = ":TSUpdate",
+  build = ':TSUpdate',
   main = 'nvim-treesitter.configs',
   opts = {
     ensure_installed = {
@@ -23,10 +23,22 @@ return {
     },
     additional_vim_regex_highlighting = false,
     highlight = { enable = false },
+    textsubjects = {
+      enable = true,
+      prev_selection = ',',
+      keymaps = {
+        ['.'] = 'textsubjects-smart',
+        [';'] = 'textsubjects-container-outer',
+        ['i;'] = 'textsubjects-container-inner',
+      },
+    },
   },
   init = function()
     vim.uv.new_timer():start(300, 0, vim.schedule_wrap(function()
       vim.cmd 'TSEnable highlight'
     end))
-  end
+  end,
+  dependencies = {
+    'RRethy/nvim-treesitter-textsubjects',
+  }
 }
