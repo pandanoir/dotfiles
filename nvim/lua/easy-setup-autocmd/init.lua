4e53a2cbec766c9d99f98929c8cea5597027893f
pandanoir <pandanoir@ymail.ne.jp>
2024-06-10 22:48:17 +0900

    fix

--- nvim/lua/easy-setup-autocmd/init.lua
+++ nvim/lua/easy-setup-autocmd/init.lua
@@ -1,7 +1,6 @@
 local M = {}
+local group = vim.api.nvim_create_augroup('MyAutoCmd', { clear = true })
 function M.setup_autocmd(autocmds)
-  local group = vim.api.nvim_create_augroup('MyAutoCmd', { clear = true })
-
   for event_str, opts in pairs(autocmds) do
     opts.group = group
     local events = vim.split(event_str, ',')
