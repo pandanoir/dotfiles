182b5934b4922ac4e1c0ae6f12bb05a4770f786e
pandanoir <pandanoir@ymail.ne.jp>
2024-09-28 21:34:44 +0900

    add Edit temp file

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -5,10 +5,21 @@ return {
   opts = function()
     local dashboard = require 'alpha.themes.dashboard'
 
+
+    vim.cmd [[command! EditTempFile lua EditTempFile()]]
+    function EditTempFile()
+      local handle = io.popen('date +%Y-%m-%d')
+      local filepath = '/tmp/' .. handle:read('*a'):gsub('%s+', '') .. '.txt'
+      handle:close()
+
+      vim.cmd('e ' .. filepath)
+    end
+
     dashboard.section.buttons.val = {
       dashboard.button('o', ' ' .. ' Oil', ':Oil --float<cr>'),
       dashboard.button('f', ' ' .. ' Find file', '<cmd> Telescope find_files <cr>'),
       dashboard.button('i', ' ' .. ' New file', '<cmd> ene <BAR> startinsert <cr>'),
+      dashboard.button('t', ' ' .. ' Edit temp file', '<cmd> EditTempFile <cr>'),
       dashboard.button('r', ' ' .. ' Recent files', '<cmd> Telescope oldfiles <cr>'),
       dashboard.button('g', ' ' .. ' Find text', '<cmd> Telescope live_grep <cr>'),
       dashboard.button('l', '󰒲 ' .. ' Lazy', '<cmd> Lazy <cr>'),
