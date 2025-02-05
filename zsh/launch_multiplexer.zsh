5526d144ab666746df662da4a7d982f3173a6246
pandanoir <pandanoir@ymail.ne.jp>
2025-02-05 23:47:07 +0900

    refacotr: rename launch_tmux to launch_multiplexer

---
+++ zsh/launch_multiplexer.zsh
@@ -0,0 +1,31 @@
+if [[ "$MULTIPLEXERS" == "tmux" ]] &&
+  command_exists tmux &&
+  [ $(whoami) != "root" ] &&
+  [[ "$TMUX_AUTO_LAUNCH" == "true" ]] &&
+  is_empty_string "$TMUX" &&
+  is_empty_string "$STY" &&
+  is_empty_string $IS_VSCODE &&
+  is_empty_string $GITHUB_ACTIONS; then
+  # アタッチされていないセッションがあったらアタッチする
+  if ! is_empty_string "$(tmux list-sessions | grep -v popup | grep -v '(attached)')"; then
+    exec tmux attach
+  elif [ -v NOT_NEW_SESSION_TMUX ]; then
+    # 新しいセッションを立ち上げたくない
+    echo "detached tmux session doesn't exist"
+  else
+    exec tmux new-session
+  fi
+  exit
+fi
+
+if [[ "$MULTIPLEXERS" == "zellij" ]] && command_exists zellij && [[ -z "$ZELLIJ" ]]; then
+  if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+    zellij attach -c main
+  else
+    zellij
+  fi
+
+  if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+    exit
+  fi
+fi
