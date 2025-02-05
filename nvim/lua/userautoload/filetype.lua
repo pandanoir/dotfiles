e7fbbdf36ec700ea7c5f19d743ef257216bd21be
pandanoir <pandanoir@ymail.ne.jp>
2025-02-06 01:27:35 +0900

    refactor: don't set zsh filetype manually

--- nvim/lua/userautoload/filetype.lua
+++ nvim/lua/userautoload/filetype.lua
@@ -10,7 +10,6 @@ end
 
 add_filetype_autocmd('nginx.conf', 'setf nginx')
 add_filetype_autocmd('/etc/nginx/*', 'setf nginx')
-add_filetype_autocmd('{,.}{zprofile,zshrc}{,.local}', 'setf zsh')
 add_filetype_autocmd('{,.}{bash_profile,bashrc}{,.local}', 'setf bash')
 add_filetype_autocmd('*.scss', 'set autoindent noexpandtab tabstop=4 shiftwidth=4')
 add_filetype_autocmd('*.vue', 'setlocal commentstring=<!--%s-->')
