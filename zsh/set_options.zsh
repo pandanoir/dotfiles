45e2664b05dff90fa7ba39ed6c59f2a269e2c5aa
pandanoir <pandanoir@ymail.ne.jp>
2025-02-05 23:49:02 +0900

    refactor: extract set_options.zsh from zshrc

---
+++ zsh/set_options.zsh
@@ -0,0 +1,17 @@
+# emacsのtrampがタイムアウトするのに対応
+[[ $TERM == "dumb" ]] && unsetopt zle && PS1='$ '
+
+bindkey -e # キーバインディングをEmacsモードに設定する
+
+autoload -U colors
+colors # 色に関連する変数(fg、bg、fx など)を設定して色付きのプロンプトやスクリプトを使えるようにする
+
+setopt no_beep  # ビープ音を消す
+setopt globdots # 明確なドットの指定なしで.から始まるファイルをマッチ
+
+# コマンド履歴関連
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt share_history
+SAVEHIST=100
+HISTFILE=$HOME/.zsh_history
