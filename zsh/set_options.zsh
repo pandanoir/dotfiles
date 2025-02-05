e35275f7860e9fbfa109a7b1d36f7cc44449d70d
pandanoir <pandanoir@ymail.ne.jp>
2025-02-06 01:15:48 +0900

    refactor: merge set_prompt and set_options

--- zsh/set_options.zsh
+++ zsh/set_options.zsh
@@ -17,3 +17,24 @@ setopt share_history
 SAVEHIST=100
 HISTFILE=$HOME/.zsh_history
 
+# プロンプト
+git-prompt() {
+  local branchname
+  branchname=$(git symbolic-ref --short HEAD 2>/dev/null)
+  if [ -z $branchname ]; then
+    return
+  fi
+  echo "%{${fg[magenta]}%}${branchname}%{${reset_color}%}"
+}
+prompt-set() {
+  local user_host='%F{074}[%n@%m]'
+  local last_status='%F{red}$([[ $? -ne 0 && $? -ne 130 ]] && echo "[%?]")' # 異常終了のとき(Ctrl-cを除く)にステータスを表示する
+  PROMPT="$user_host$last_status%f# "
+  RPROMPT='`git-prompt`%F{048}[%~]%f'
+}
+prompt-reset() {
+  PROMPT="# "
+  RPROMPT=''
+}
+prompt-set
+setopt prompt_subst #表示毎にPROMPTで設定されている文字列を評価する
