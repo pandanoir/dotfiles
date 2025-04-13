53da000c629a304ff231270bc4ea658508b444e5
pandanoir <pandanoir@ymail.ne.jp>
2025-04-13 11:43:53 +0900

    refactor: sort config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,42 +11,6 @@ local ensure_installed = {
 }
 
 return {
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    config = true,
-  },
-  {
-    'glepnir/lspsaga.nvim',
-    event = 'BufRead',
-    opts = {
-      finder = {
-        keys = {
-          toggle_or_open = '<CR>',
-          quit = { 'q', '<ESC>' },
-        },
-      },
-    },
-    init = function()
-      require 'easy-setup-autocmd'.setup_autocmd {
-        ['LspAttach'] = {
-          callback = function(ev)
-            local map = vim.keymap.set
-            local opt = function(desc)
-              return { silent = true, buffer = ev.buf, desc = desc }
-            end
-            map('n', 'gd', '<cmd>Lspsaga goto_definition<CR>', opt())
-            map('n', 'gp', '<cmd>Lspsaga peek_definition<CR>', opt())
-            map('n', 'grr', '<cmd>Lspsaga finder<CR>', opt())
-            map('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opt())
-            map('n', 'grn', '<cmd>Lspsaga rename<CR>', opt('rename using LSP'))
-            map('n', 'gra', '<cmd>Lspsaga code_action<CR>', opt('open code action'))
-            map('n', '<leader>o', '<cmd>Lspsaga outline<CR>', opt('show outline'))
-          end
-        }
-      }
-    end
-  },
   -- language serverのインストールと設定を行う
   {
     'williamboman/mason-lspconfig.nvim',
@@ -92,5 +56,41 @@ return {
       },
       'neovim/nvim-lspconfig',
     }
+  },
+  {
+    'glepnir/lspsaga.nvim',
+    event = 'BufRead',
+    opts = {
+      finder = {
+        keys = {
+          toggle_or_open = '<CR>',
+          quit = { 'q', '<ESC>' },
+        },
+      },
+    },
+    init = function()
+      require 'easy-setup-autocmd'.setup_autocmd {
+        ['LspAttach'] = {
+          callback = function(ev)
+            local map = vim.keymap.set
+            local opt = function(desc)
+              return { silent = true, buffer = ev.buf, desc = desc }
+            end
+            map('n', 'gd', '<cmd>Lspsaga goto_definition<CR>', opt())
+            map('n', 'gp', '<cmd>Lspsaga peek_definition<CR>', opt())
+            map('n', 'grr', '<cmd>Lspsaga finder<CR>', opt())
+            map('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opt())
+            map('n', 'grn', '<cmd>Lspsaga rename<CR>', opt('rename using LSP'))
+            map('n', 'gra', '<cmd>Lspsaga code_action<CR>', opt('open code action'))
+            map('n', '<leader>o', '<cmd>Lspsaga outline<CR>', opt('show outline'))
+          end
+        }
+      }
+    end
+  },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    config = true,
   },
 }
