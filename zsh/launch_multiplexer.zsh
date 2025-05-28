d519307ce6df45261c271069a7de688015b5014c
pandanoir <pandanoir@ymail.ne.jp>
2025-05-28 20:24:11 +0900

    attach to detached session excluding popup one

--- zsh/launch_multiplexer.zsh
+++ zsh/launch_multiplexer.zsh
@@ -5,14 +5,18 @@ is_terminal_app() {
   is_empty_string $GITHUB_ACTIONS
 }
 
-if is_terminal_app &&
-  [[ "$MULTIPLEXER" == "tmux" ]] &&
+if ! is_terminal_app; then
+  return
+fi
+
+if [[ "$MULTIPLEXER" == "tmux" ]] &&
   command_exists tmux &&
   is_empty_string "$TMUX" &&
   [[ "$TMUX_AUTO_LAUNCH" == "true" ]]; then
-  # アタッチされていないセッションがあったらアタッチする
-  if ! is_empty_string "$(tmux list-sessions | grep -v popup | grep -v '(attached)')"; then
-    exec tmux attach
+  # アタッチされていないセッション(popup以外)があったらアタッチする
+  detached_session="$(tmux list-sessions -f '#{?session_attached,0,1}' -F '#S' | grep -v popup | head -n 1)"
+  if ! is_empty_string "$detached_session"; then
+    exec tmux attach -t "$detached_session"
   elif [ -v NOT_NEW_SESSION_TMUX ]; then
     # 新しいセッションを立ち上げたくない
     echo "detached tmux session doesn't exist"
@@ -22,8 +26,7 @@ if is_terminal_app &&
   exit
 fi
 
-if is_terminal_app &&
-  [[ "$MULTIPLEXER" == "zellij" ]] &&
+if [[ "$MULTIPLEXER" == "zellij" ]] &&
   command_exists zellij &&
   is_empty_string "$ZELLIJ"; then
   if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
