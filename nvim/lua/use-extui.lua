710760f86fee7ba550b1db087f878ccf492b980f
pandanoir <pandanoir@ymail.ne.jp>
2025-06-20 01:07:27 +0900

    change for #34378
    https://github.com/neovim/neovim/pull/34378

--- nvim/lua/use-extui.lua
+++ nvim/lua/use-extui.lua
@@ -1,13 +1,11 @@
 -- extuiが使えたら設定する
 local ok, extui = pcall(require, 'vim._extui')
 if ok then
-  extui.enable({
+  extui.enable {
     enable = true,
     msg = {
-      pos = 'cmd',
-      box = {
-        timeout = 4000,
-      },
+      target = 'cmd',
+      timeout = 4000,
     },
-  })
+  }
 end
