98e1d2018019786f63ddae276a0b23d84ff9ac65
pandanoir <pandanoir@ymail.ne.jp>
2025-09-25 23:29:41 +0900

    add luasnip

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,12 +2,19 @@ return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
+    { 'L3MON4D3/LuaSnip', dependencies = { 'rafamadriz/friendly-snippets' } },
+    'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
   },
   opts = function()
     local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    luasnip.filetype_extend('typescript', { 'javascript' })
+    luasnip.filetype_extend('typescriptreact', { 'javascript' })
+    require 'luasnip.loaders.from_vscode'.lazy_load()
+
     return {
       enabled = true,
       mapping = cmp.mapping.preset.insert {
@@ -18,10 +25,26 @@ return {
         ['<Tab>'] = function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
           else
             fallback()
           end
-        end
+        end,
+        ['<S-Tab>'] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
       window = {
         completion = cmp.config.window.bordered(),
@@ -29,6 +52,7 @@ return {
       },
       sources = cmp.config.sources {
         { name = 'nvim_lsp' },
+        { name = 'luasnip' },
         { name = 'buffer' },
         { name = 'path' },
       },
