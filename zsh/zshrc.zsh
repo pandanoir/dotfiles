8de8ce2413f21184f1d15aaeb212ba3a93a2a0d3
Naoto Ikuno <pandanoir@ymail.ne.jp>
2025-12-23 19:51:19 +0900

    fix: sort order of keybind.zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -4,10 +4,10 @@ source_notify "$ZDOTDIR/launch_multiplexer.zsh"
 
 source_notify "$ZDOTDIR/set_options.zsh"
 source_notify "$ZDOTDIR/alias.zsh"
-source_notify "$ZDOTDIR/keybind.zsh"
 source_notify "$ZDOTDIR/init_completion.zsh"
 source_notify_if_exists "$XDG_CACHE_HOME/fzf/shell/completion.zsh"
 source_notify_if_exists "$XDG_CACHE_HOME/fzf/shell/key-bindings.zsh"
+source_notify "$ZDOTDIR/keybind.zsh"
 source_notify "$ZDOTDIR/zinit.zsh"
 
 source_notify_if_exists "$ZDOTDIR/.zshrc.local"
