42bd7eedf299d7d233c207fd53edc011b2a30575
pandanoir <pandanoir@ymail.ne.jp>
2026-04-19 12:21:25 +0900

    fix: treesitterパッチを追加

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -41,6 +41,22 @@ return {
       },
     },
     init = function()
+      -- nvim-treesitter master ブランチ(archived)互換パッチ:
+      -- 0.13+ では query directive/predicate の match[capture_id] が TSNode から TSNode[] に変わったが、
+      -- master ブランチの query_predicates は単一 TSNode 前提のため markdown injection 等で
+      -- `attempt to call method 'range' (a nil value)` エラーになる。
+      -- get_node_text に渡された table を最後の要素に正規化することで吸収する。
+      do
+        local orig = vim.treesitter.get_node_text
+        ---@diagnostic disable-next-line: duplicate-set-field
+        vim.treesitter.get_node_text = function(node, source, opts)
+          if type(node) == 'table' and node[1] then
+            node = node[#node]
+          end
+          return orig(node, source, opts)
+        end
+      end
+
       vim.uv.new_timer():start(300, 0, vim.schedule_wrap(function()
         vim.cmd 'TSEnable highlight'
         require 'improve-default-scheme'.improve()
