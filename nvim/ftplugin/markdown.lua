8ed1c0dfc4d9e63610b23e0872af7dd0ea1a79b2
pandanoir <pandanoir@ymail.ne.jp>
2026-04-26 19:26:46 +0900

    fix: markdownでtreesitterを止める

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,8 +1,7 @@
 -- claude promptでフォーマッターを走らせたくない
 if require('env').from_claude then
-  -- markdown & formatter & treesitter 全部止める
-  vim.bo.filetype = ''
   vim.b.disable_autoformat = true
   vim.bo.formatexpr = ''
   vim.bo.formatprg = ''
+  vim.treesitter.stop(0)
 end
