f52722cbae2ab5dffbd215c04875683b9b313fa5
pandanoir <pandanoir@ymail.ne.jp>
2026-04-28 08:31:17 +0900

    feat: quicker.nvimのexpand/collapseキーマップを追加

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -365,7 +365,20 @@ return {
     ft = 'qf',
     ---@module 'quicker'
     ---@type quicker.SetupOptions
-    opts = {},
+    opts = {
+      keys = {
+        {
+          '>',
+          function() require 'quicker'.expand { before = 2, after = 2, add_to_existing = true } end,
+          desc = 'Expand quickfix context',
+        },
+        {
+          '<',
+          function() require 'quicker'.collapse() end,
+          desc = 'Collapse quickfix context',
+        },
+      },
+    },
   },
   { -- 対応する括弧のマッチング強化
     'andymass/vim-matchup',
