19a9d792bd1e7903e3bd7c409048eaf0b806871b
pandanoir <pandanoir@ymail.ne.jp>
2026-04-28 18:52:04 +0900

    fix: 使ってないカラースキームを削除

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,5 @@
 return {
   {
-    'navarasu/onedark.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require 'onedark'.setup {
-        style = 'deep',
-      }
-
-      -- カラースキームが未設定ならonedarkに設定
-      local current_colorscheme = vim.g.colors_name
-      if current_colorscheme == nil or current_colorscheme == '' then
-        require 'onedark'.load()
-      end
-    end
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
     'catppuccin/nvim',
     name = 'catppuccin',
     lazy = false,
