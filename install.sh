120f48854c7167fb24dfd13a7ad4d1cb47a8983d
pandanoir <pandanoir@ymail.ne.jp>
2026-05-02 21:44:41 +0900

    refactor: simplify

--- install.sh
+++ install.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 # curl -sL dot.pandanoir.net | sh
 set -eu
-
-command_exists() { type "$1" >/dev/null 2>&1; }
-dir_exists() { [ -d $1 ]; }
 info() { echo -e "\033[0;34m[INFO]\033[0;39m $1"; }
 warn() { echo -e "\033[0;33m[WARN]\033[0;39m $1"; }
-
 DOTDIR="$HOME/dotfiles"
 
-if ! command_exists git; then
-  warn "you must install git!"
-  exit 1
+if ! type git >/dev/null 2>&1; then
+  warn "you must install git!"; exit 1
 fi
-if ! dir_exists "$DOTDIR"; then
+if ! [ -d "$DOTDIR" ]; then
   info "download dotfiles"
   git clone https://github.com/pandanoir/dotfiles "$DOTDIR"
 fi
 
 source "$DOTDIR/_scripts/check_requirements.sh"
 source "$DOTDIR/_scripts/install_tools.sh"
-source "$DOTDIR/_scripts/deploy.sh"
+source "$DOTDIR/_scripts/deploy_files.sh"
 source "$DOTDIR/_scripts/init_config.sh"
 
 install_tools
 check_requirements
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ] || [ "$1" = "deploy" ] || [ "$1" = "-d" ]; then
   deploy
-  init_config
-elif [ "$1" = "deploy" ] || [ "$1" = "-d" ]; then
-  deploy
-elif [ "$1" = "init" ] || [ "$1" = "-i" ]; then
+fi
+if [ $# -eq 0 ] || [ "$1" = "init" ] || [ "$1" = "-i" ]; then
   init_config
 fi
